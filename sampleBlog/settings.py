67c376128e5658201a50a9dbf47726c4412fafc2
hafis <hafismuhammed25@gmail.com>
2020-11-29 11:59:12 +0530

    heroku settings

--- sampleBlog/settings.py
+++ sampleBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -191,3 +192,6 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 SITE_ID = 1
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
